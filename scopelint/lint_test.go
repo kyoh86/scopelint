2945379372dc30679d5dc4fffe1202884b2e0955
Kyoichiro Yamada <me@kyoh86.dev>
2019-11-10 00:26:16 +0900

    wip: create model and test

--- scopelint/lint_test.go
+++ scopelint/lint_test.go
@@ -56,4 +56,178 @@ func returning() *int {
 		require.NoError(t, err)
 		assert.Empty(t, problems)
 	})
+
+	t.Run("issue #4", func(t *testing.T) {
+
+		t.Run("positive", func(t *testing.T) {
+			l := new(Linter)
+			problems, err := l.Lint("mypkg/mypkg.go", []byte(`package main
+import "testing"
+
+func TestSomething(t *testing.T) {
+	for _, tc := range []struct {
+		expected    string
+	}{} {
+		t.Run("sub", func(t *testing.T) { // :memo: t.Run runs sub func immediately
+			if "result" != tc.expected {
+				t.Fatal("failed")
+			}
+		})
+	}
+}`))
+
+			require.NoError(t, err)
+			if assert.Len(t, problems, 1) {
+				assert.Equal(t, "Using the variable on range scope \"tc\" in function literal", problems[0].Text)
+			}
+		})
+
+		t.Run("ignore line", func(t *testing.T) {
+			l := new(Linter)
+			problems, err := l.Lint("mypkg/mypkg.go", []byte(`package main
+
+import "testing"
+
+func TestSomething(t *testing.T) {
+	for _, tc := range []struct {
+		expected    string
+	}{} {
+		t.Run("sub", func(t *testing.T) { // :memo: t.Run runs sub func immediately
+			t.Log(tc.expected) //scopelint:ignore // "result" != tc.expected
+			if "result" != tc.expected {
+				t.Fatal("failed")
+			}
+		})
+	}
+}`))
+
+			require.NoError(t, err)
+			if assert.Len(t, problems, 2) {
+				assert.True(t, problems[0].Ignored, "%#v", problems[0])
+				assert.False(t, problems[1].Ignored, "%#v", problems[1])
+			}
+		})
+
+		t.Run("ignore block", func(t *testing.T) {
+			l := new(Linter)
+			problems, err := l.Lint("mypkg/mypkg.go", []byte(`package main
+import "testing"
+
+func TestSomething(t *testing.T) {
+	for _, tc := range []struct {
+		expected    string
+	}{} {
+		t.Run("sub", func(t *testing.T) { // :memo: t.Run runs sub func immediately
+			//scopelint:ignore
+			if "result" != tc.expected {
+				t.Fatal("failed")
+			}
+		})
+	}
+}`))
+
+			require.NoError(t, err)
+			if assert.Len(t, problems, 1) {
+				assert.True(t, problems[0].Ignored, "%#v", problems[0])
+			}
+		})
+
+		t.Run("ignore block with other comments", func(t *testing.T) {
+			l := new(Linter)
+			problems, err := l.Lint("mypkg/mypkg.go", []byte(`package main
+import "testing"
+
+func TestSomething(t *testing.T) {
+	for _, tc := range []struct {
+		expected    string
+	}{} {
+		t.Run("sub", func(t *testing.T) { // :memo: t.Run runs sub func immediately
+			//scopelint:ignore
+			// compare expected and result
+			if "result" != tc.expected {
+				t.Fatal("failed")
+			}
+		})
+	}
+}`))
+
+			require.NoError(t, err)
+			if assert.Len(t, problems, 1) {
+				assert.True(t, problems[0].Ignored, "%#v", problems[0])
+			}
+		})
+
+		t.Run("ignore ancestor block", func(t *testing.T) {
+			l := new(Linter)
+			problems, err := l.Lint("mypkg/mypkg.go", []byte(`package main
+import "testing"
+
+//scopelint:ignore
+func TestSomething(t *testing.T) {
+	for _, tc := range []struct {
+		expected    string
+	}{} {
+		t.Run("sub", func(t *testing.T) { // :memo: t.Run runs sub func immediately
+			if "result" != tc.expected {
+				t.Fatal("failed")
+			}
+		})
+	}
+}`))
+
+			require.NoError(t, err)
+			if assert.Len(t, problems, 1) {
+				assert.True(t, problems[0].Ignored, "%#v", problems[0])
+			}
+		})
+		t.Run("ignore file", func(t *testing.T) {
+			l := new(Linter)
+			problems, err := l.Lint("mypkg/mypkg.go", []byte(`//scopelint:ignore
+package main
+
+import "testing"
+
+func TestSomething(t *testing.T) {
+	for _, tc := range []struct {
+		expected    string
+	}{} {
+		t.Run("sub", func(t *testing.T) { // :memo: t.Run runs sub func immediately
+			if "result" != tc.expected {
+				t.Fatal("failed")
+			}
+		})
+	}
+}`))
+
+			require.NoError(t, err)
+			if assert.Len(t, problems, 1) {
+				assert.True(t, problems[0].Ignored, "%#v", problems[0])
+			}
+		})
+
+		t.Run("positive in next one of ignored line", func(t *testing.T) {
+			l := new(Linter)
+			problems, err := l.Lint("mypkg/mypkg.go", []byte(`package main
+import "testing"
+
+func TestSomething(t *testing.T) {
+	for _, tc := range []struct {
+		expected    string
+	}{} {
+		t.Run("sub", func(t *testing.T) { // :memo: t.Run runs sub func immediately
+			t.Log(tc.expected) //scopelint:ignore
+			if "result" != tc.expected {
+				t.Fatalf("failed")
+			}
+		})
+	}
+}`))
+
+			require.NoError(t, err)
+			if assert.Len(t, problems, 2) {
+				assert.True(t, problems[0].Ignored, "%#v", problems[0])  // t.Log ~
+				assert.False(t, problems[1].Ignored, "%#v", problems[1]) // if ~
+			}
+		})
+	})
 }
